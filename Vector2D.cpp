47847b391c30ec50acb3f595a1747481f051584c
JOGUI22 <JORDICASA22@gmail.com>
2014-01-15 23:29:55 +0100

    Added version 1 of class Vector2D with basic behaviour(NO BUG)

---
+++ Vector2D.cpp
@@ -0,0 +1,99 @@
+#include "Vector2D.h"
+
+Vector2D::Vector2D()
+{
+    m_x = 0;
+    m_y = 0;
+}
+
+Vector2D::Vector2D(float x, float y)
+{
+    m_x = x;
+    m_y = y;
+}
+
+Vector2D Vector2D::operator+(const Vector2D& v2)
+{
+    return Vector2D(m_x + v2.m_x, m_y + v2.m_y);
+}
+
+Vector2D Vector2D::operator+=(const Vector2D& v2)
+{
+    m_x += v2.m_x;
+    m_y += v2.m_y;
+
+    return *this; //we return the self object.
+}
+
+Vector2D Vector2D::operator-(const Vector2D& v2)
+{
+    return Vector2D(m_x - v2.m_x, m_y - v2.m_y);
+}
+
+Vector2D Vector2D::operator-=(const Vector2D& v2)
+{
+    m_x -= v2.m_x;
+    m_y -= v2.m_y;
+
+    return *this;
+}
+
+Vector2D Vector2D::operator*(float scalar)
+{
+    return Vector2D(m_x * scalar, m_y * scalar);
+}
+
+Vector2D Vector2D::operator*=(float scalar)
+{
+    m_x *= scalar;
+    m_y *= scalar;
+
+    return *this;
+}
+
+Vector2D Vector2D::operator/(float scalar)
+{
+    return Vector2D(m_x / scalar, m_y / scalar);
+}
+
+Vector2D Vector2D::operator/=(float scalar)
+{
+    m_x /= scalar;
+    m_y /= scalar;
+
+    return *this;
+}
+
+float Vector2D::getX() const
+{
+    return m_x;
+}
+
+float Vector2D::getY() const
+{
+    return m_y;
+}
+
+float Vector2D::length() const
+{
+    return sqrt(m_x * m_x + m_y * m_y);
+}
+
+void Vector2D::setX(float x)
+{
+    m_x = x;
+}
+
+void Vector2D::setY(float y)
+{
+    m_y = y;
+}
+
+void Vector2D::normalize()
+{
+    float aux_length = length();
+    if (aux_length > 0) //don't divide 0!
+    {
+        (*this) *= 1 / aux_length;
+    }
+}
---
+++ Vector2D.h
@@ -0,0 +1,41 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+#include<math.h>
+
+class Vector2D
+{
+    public:
+    //Constructors
+        Vector2D();
+        Vector2D(float x, float y);
+
+    //Operators
+        Vector2D operator+(const Vector2D& v2);
+        Vector2D operator+=(const Vector2D& v2);
+        Vector2D operator-(const Vector2D& v2);
+        Vector2D operator-=(const Vector2D& v2);
+        Vector2D operator*(float scalar);
+        Vector2D operator*=(float scalar);
+        Vector2D operator/(float scalar);
+        Vector2D operator/=(float scalar);
+
+
+    //GETs
+        float getX() const;
+        float getY() const;
+        float length() const; //lenght of the vector = sqrt(x²+y²)
+
+    //Methods
+        void setX(float x); //change x value
+        void setY(float y); //change y value
+
+        void normalize(); //make vector length = 1
+
+    private:
+
+        float m_x;
+        float m_y;
+};
+
+#endif // VECTOR2D_H
