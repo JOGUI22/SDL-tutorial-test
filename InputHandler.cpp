27f6065e774904acff928c9b22787ceb4ad00913
JOGUI22 <JORDICASA22@gmail.com>
2014-01-17 22:04:08 +0100

    Added InputHandler Singleton class and basic joystick handling(fix bug)

---
+++ InputHandler.cpp
@@ -0,0 +1,74 @@
+#include "InputHandler.h"
+#include "Game.h"
+
+InputHandler* InputHandler::s_pInstance = 0; //IMPORTANT TO MAKE SINGLETON WORK
+
+InputHandler::InputHandler()
+{
+    //ctor
+}
+
+InputHandler::~InputHandler()
+{
+
+}
+
+void InputHandler::initialiseJoysticks()
+{
+    if(SDL_WasInit(SDL_INIT_JOYSTICK) == 0) //check if SDL_INIT_JOYSTICK subsystem is initiate
+    {
+        SDL_InitSubSystem(SDL_INIT_JOYSTICK); //initiate SDL_INIT_JOYSTICK
+    }
+
+    if(SDL_NumJoysticks() > 0) //if there're joysticks...
+    {
+        for (int i = 0; i < SDL_NumJoysticks(); i++) //for each joystick
+        {
+            SDL_Joystick* joy = SDL_JoystickOpen(i);
+            if (SDL_JoystickOpen(i) != NULL) // try to activate the joystick, otherwise show the error
+            {
+                m_joysticks.push_back(joy);
+            }
+            else
+            {
+                cout << SDL_GetError();
+            }
+        }
+        SDL_JoystickEventState(SDL_ENABLE); //start listening joystick events
+        m_bJoysticksInitialised == true;
+
+        cout << "Initialised " << m_joysticks.size() << " joystick(s)" << endl;
+    }
+    else
+    {
+        m_bJoysticksInitialised = false;
+    }
+}
+
+bool InputHandler::joysticksInitialised()
+{
+    return m_bJoysticksInitialised;
+}
+
+void InputHandler::clean()
+{
+    if(m_bJoysticksInitialised)
+    {
+        for(int i= 0; i < SDL_NumJoysticks(); i++)
+        {
+            SDL_JoystickClose(m_joysticks[i]);
+        }
+    }
+}
+
+void InputHandler::update()
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            TheGame::Instance()->quit();
+        }
+    }
+}
---
+++ InputHandler.h
@@ -0,0 +1,46 @@
+#ifndef INPUTHANDLER_H
+#define INPUTHANDLER_H
+
+#include "SDL2/SDL.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class InputHandler
+{
+    public:
+
+        static InputHandler* Instance()  // ** begin ** SINGLETON PUBLIC CREATOR
+        {
+            if(s_pInstance == 0)
+            {
+                s_pInstance = new InputHandler();
+            }
+
+            return s_pInstance; // ** end ** SINGLETON PUBLIC CREATOR
+        }
+
+        void update();
+        void clean(); //destructor
+
+        void initialiseJoysticks(); //pre: -- //post: initialise joystick handling
+        bool joysticksInitialised(); //pre: -- //post: return if joysticks are initialised
+
+    private:
+
+        //Atribute members
+        vector<SDL_Joystick* > m_joysticks; //all the connected joysticks
+        bool m_bJoysticksInitialised; //are joystick initialisated?
+
+        //Private methods
+        InputHandler(); //SINGLETON private constructor
+        ~InputHandler(); //SINGLETON private destructor
+
+        //SINGLETON
+        static InputHandler* s_pInstance; //the own instance
+};
+
+typedef InputHandler TheInputHandler; //only one singleton in all the program
+
+#endif // INPUTHANDLER_H
