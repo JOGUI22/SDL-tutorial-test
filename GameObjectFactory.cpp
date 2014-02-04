0506431aeeef1d52a75a21eab04369f9e53ef112
JOGUI22 <JORDICASA22@gmail.com>
2014-02-04 20:33:31 +0100

    First implementation of GameObjectFactory

---
+++ GameObjectFactory.cpp
@@ -0,0 +1,32 @@
+#include "GameObjectFactory.h"
+
+GameObjectFactory* GameObjectFactory::s_pInstance = 0;
+
+bool GameObjectFactory::registerType(string typeID, BaseCreator* pCreator)
+{
+    map<string, BaseCreator*>::iterator it = m_creators.find(typeID);
+
+    //if the type is already registered, do nothing
+    if(it != m_creators.end())
+    {
+        delete pCreator;
+        return false;
+    }
+
+    m_creators[typeID] = pCreator;
+    return true;
+}
+
+GameObject* GameObjectFactory::create(string typeID)
+{
+    map<string, BaseCreator*>::iterator it = m_creators.find(typeID);
+
+    if(it == m_creators.end())
+    {
+        cout << "could not find type: " << typeID << endl;
+        return NULL;
+    }
+
+    BaseCreator* pCreator = it->second;
+    return pCreator->createGameObject();
+}
